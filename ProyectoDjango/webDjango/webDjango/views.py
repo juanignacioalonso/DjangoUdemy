b86ccd822e5db439182e37e43b9f0549070c05e0
Juan Ignacio <zalbak87@gmail.com>
2022-11-16 11:02:20 -0300

    video 97

--- ProyectoDjango/webDjango/webDjango/views.py
+++ ProyectoDjango/webDjango/webDjango/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+#recibe tres argumentos, el primero es a peticion, el segundo el nombre de nuestro archivo html, el ultimo es cun diccionario de clave valor
 def saludo(request):
-    return render(request,"index.html",{})#recibe tres argumentos, el primero es a peticion, el segundo el nombre de nuestro archivo html, el ultimo es cun diccionario de clave valor
+    return render(request,"index.html",{
+        'mensaje':'Este es un mensaje de mi función'
+    })

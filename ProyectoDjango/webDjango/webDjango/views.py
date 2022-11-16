5bab2a8e766f1a04a136626f137d3b3c2f5c90e1
Juan Ignacio <zalbak87@gmail.com>
2022-11-16 15:08:10 -0300

    Video 98

--- ProyectoDjango/webDjango/webDjango/views.py
+++ ProyectoDjango/webDjango/webDjango/views.py
@@ -4,5 +4,12 @@ from django.shortcuts import render
 #recibe tres argumentos, el primero es a peticion, el segundo el nombre de nuestro archivo html, el ultimo es cun diccionario de clave valor
 def saludo(request):
     return render(request,"index.html",{
-        'mensaje':'Este es un mensaje de mi función'
+        'titulo': 'Personas',
+        'mensaje':'Ingreso',
+        'personas':[
+            {'titulo':'Maria','edad':15,'adulto':False},
+            {'titulo':'Matias','edad':18,'adulto':True},
+            {'titulo':'Jorge','edad':11,'adulto':False},
+            {'titulo':'Marta','edad':44,'adulto':True}
+        ]
     })
